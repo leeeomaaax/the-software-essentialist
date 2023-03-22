614bb762e940cdde34446c3947458f62e6764808
leomax <leomax89@gmail.com>
2023-03-22 10:39:24 -0300

    [refactor] improves tests

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -8,15 +8,23 @@ describe("fizzbuzz", () => {
     expect(() => fizzbuzz(101)).toThrow()
   })
   it("should return the same number as string if not a multiple of 3 or 5", () => {
-    expect(fizzbuzz(1)).toEqual("1")
+    Array.from({ length: 100 }, (_, i) => i + 1)
+      .filter((number) => number % 3 !== 0 && number % 5 !== 0)
+      .map((number) => expect(fizzbuzz(number)).toEqual(number.toString()))
   })
   it('should return a "Fizz" if multiples of 3', () => {
-    expect(fizzbuzz(3)).toEqual("Fizz")
+    Array.from({ length: 100 }, (_, i) => i + 1)
+      .filter((number) => number % 3 === 0 && number % 5 !== 0)
+      .map((number) => expect(fizzbuzz(number)).toEqual("Fizz"))
   })
   it('should return a "Buzz" if multiples of 5', () => {
-    expect(fizzbuzz(5)).toEqual("Buzz")
+    Array.from({ length: 100 }, (_, i) => i + 1)
+      .filter((number) => number % 3 !== 0 && number % 5 === 0)
+      .map((number) => expect(fizzbuzz(number)).toEqual("Buzz"))
   })
   it('should return a "FizzBuzz" if multiples of 3 and 5', () => {
-    expect(fizzbuzz(15)).toEqual("FizzBuzz")
+    Array.from({ length: 100 }, (_, i) => i + 1)
+      .filter((number) => number % 3 === 0 && number % 5 === 0)
+      .map((number) => expect(fizzbuzz(number)).toEqual("FizzBuzz"))
   })
 })
