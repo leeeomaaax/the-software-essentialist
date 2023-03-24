601cbda358bf8424ef43b96672741ac570052762
leomax <leomax89@gmail.com>
2023-03-24 10:31:28 -0300

    [new] check if palindrome on word

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,19 @@
+import { isPalindrome } from "./index"
 
-describe('palindrome checker', () => {
-
+describe("isPalindrome", () => {
+  it("Check palindrome on palindrome word", () => {
+    const tests = ["mom", "wow"]
+    tests.forEach((test) => {
+      expect(isPalindrome(test)).toEqual(true)
+    })
+  })
+  it("Check palindrome on non-palindrome word", () => {
+    const tests = ["leo", "max"]
+    tests.forEach((test) => {
+      expect(isPalindrome(test)).toEqual(false)
+    })
+  })
+  it("Check palindrome on palindrome phrase", () => {})
+  it("Check palindrome on non-palindrome phrase", () => {})
+  it("Check palindrome even if case is off", () => {})
 })
