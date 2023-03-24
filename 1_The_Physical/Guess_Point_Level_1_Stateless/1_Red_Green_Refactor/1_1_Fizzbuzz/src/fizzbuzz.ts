a89f9691793b4c95750a5f431b3761ef5475a8d3
leomax <leomax89@gmail.com>
2023-03-24 09:38:06 -0300

    [refactor] readability improvement

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,9 +1,11 @@
 const fizzbuzz = (number: number): string => {
   if (number < 1 || number > 100)
     throw new Error("number should be between 1 and 100")
-  if (number % 3 === 0 && number % 5 === 0) return "FizzBuzz"
-  if (number % 3 === 0) return "Fizz"
-  if (number % 5 === 0) return "Buzz"
+  const isMultipleOf3 = number % 3 === 0
+  const isMultipleOf5 = number % 5 === 0
+  if (isMultipleOf3 && isMultipleOf5) return "FizzBuzz"
+  if (isMultipleOf3) return "Fizz"
+  if (isMultipleOf5) return "Buzz"
   return number.toString()
 }
 
