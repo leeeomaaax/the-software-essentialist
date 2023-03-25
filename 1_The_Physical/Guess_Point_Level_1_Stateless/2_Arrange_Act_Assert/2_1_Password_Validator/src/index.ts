da0772c6ec55da95437bea2a8d98e7021c9a43f2
leomax <leomax89@gmail.com>
2023-03-25 11:03:01 -0300

    [refactor] improves readability

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -4,20 +4,27 @@ type ValidatePasswordResult =
 
 export const validatePassword = (password: string): ValidatePasswordResult => {
   const errors = []
+
   if (password.length < 5) errors.push("SmallerThen5Chars")
+
   if (password.length > 15) errors.push("GreaterThen15Chars")
+
   const hasAtLeastOneDigit = password
     .split("")
     .find((char) => !isNaN(parseInt(char)))
   if (!hasAtLeastOneDigit) errors.push("NoDigit")
+
   const hasAtLeastOneUpperCase = password
     .split("")
     .find((char) => /^[A-Z]*$/.test(char))
   if (!hasAtLeastOneUpperCase) errors.push("NoUpperCase")
-  if (errors.length > 0)
+
+  const hasErrors = errors.length > 0
+  if (hasErrors)
     return {
       isValid: false,
       errors,
     }
+
   return { isValid: true }
 }
