c2cc887297b7c5d67f8807d6ee33d5e0f4070d4f
leomax <leomax89@gmail.com>
2023-04-12 10:55:21 -0300

    [refactor] improves readability

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,18 +1,31 @@
+const isValidHour = (hour: number): boolean =>
+  !isNaN(hour) && hour >= 0 && hour <= 23
+const isValidMinute = (minute: number): boolean =>
+  !isNaN(minute) && minute >= 0 && minute <= 59
+
 export const isValidMilitaryTime = (timeString: string): boolean => {
   if (timeString.length !== 13) return false
+
   const hourStart = parseInt(timeString.substring(0, 2))
-  if (isNaN(hourStart) || hourStart < 0 || hourStart > 23) return false
+  if (!isValidHour(hourStart)) return false
+
   const separatorStart = timeString.substring(2, 3)
   if (separatorStart !== ":") return false
+
   const minuteStart = parseInt(timeString.substring(3, 5))
+  if (!isValidMinute(minuteStart)) return false
+
   const separatorRange = timeString.substring(5, 8)
   if (separatorRange !== " - ") return false
-  if (isNaN(minuteStart) || minuteStart < 0 || minuteStart > 59) return false
+
   const hourEnd = parseInt(timeString.substring(8, 10))
-  if (isNaN(hourEnd) || hourEnd < 0 || hourEnd > 23) return false
+  if (!isValidHour(hourEnd)) return false
+
   const separatorEnd = timeString.substring(10, 11)
   if (separatorEnd !== ":") return false
+
   const minuteEnd = parseInt(timeString.substring(11, 13))
-  if (isNaN(minuteEnd) || minuteEnd < 0 || minuteEnd > 59) return false
+  if (!isValidMinute(minuteEnd)) return false
+
   return true
 }
