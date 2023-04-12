448e4904dd9839ee65c25fc2c586225f1c2c2a86
leomax <leomax89@gmail.com>
2023-04-12 10:15:58 -0300

    validates empty string as incorrect

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -2,10 +2,18 @@ import { isValidMilitaryTime } from "./index"
 
 describe("military time validator", () => {
   it("should validate a string as a correct military time", () => {
-    const input = ""
+    const input = "aaa"
 
     const result = isValidMilitaryTime(input)
 
     expect(result).toBeTruthy()
+  })
+
+  it("should validate a empty string as a incorrect military time", () => {
+    const input = ""
+
+    const result = isValidMilitaryTime(input)
+
+    expect(result).toBeFalsy()
   })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,3 +1,4 @@
 export const isValidMilitaryTime = (timeString: string): boolean => {
+  if (timeString.length === 0) return false
   return true
 }
