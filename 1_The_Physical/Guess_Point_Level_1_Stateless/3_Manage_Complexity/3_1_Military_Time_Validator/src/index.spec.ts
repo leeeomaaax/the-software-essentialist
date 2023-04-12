2f7767c8caede57ec099de5a96336b842e8a4105
leomax <leomax89@gmail.com>
2023-04-12 10:31:16 -0300

    validates minutes

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -2,7 +2,7 @@ import { isValidMilitaryTime } from "./index"
 
 describe("military time validator", () => {
   it("should validate a string as a correct military time", () => {
-    const input = "12"
+    const input = "12:12"
 
     const result = isValidMilitaryTime(input)
 
@@ -26,6 +26,20 @@ describe("military time validator", () => {
   })
   it("should validate hour '24' in string as a correct", () => {
     const input = "24:00 - 00:00"
+
+    const result = isValidMilitaryTime(input)
+
+    expect(result).toBeFalsy()
+  })
+  it("should validate minutes in string as a correct", () => {
+    const input = "00:15 - 00:00"
+
+    const result = isValidMilitaryTime(input)
+
+    expect(result).toBeTruthy()
+  })
+  it("should validate minute '60' in string as a correct", () => {
+    const input = "00:60 - 00:00"
 
     const result = isValidMilitaryTime(input)
 
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -2,5 +2,7 @@ export const isValidMilitaryTime = (timeString: string): boolean => {
   if (timeString.length === 0) return false
   const hour = parseInt(timeString.substring(0, 2))
   if (isNaN(hour) || hour < 0 || hour > 23) return false
+  const minute = parseInt(timeString.substring(3, 5))
+  if (isNaN(minute) || minute < 0 || minute > 59) return false
   return true
 }
