d7b02b1e11ab4d3d712952b51b8b16798089f4bb
leomax <leomax89@gmail.com>
2023-04-12 10:36:44 -0300

    validates hourEnd and minuteEnd

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -2,7 +2,7 @@ import { isValidMilitaryTime } from "./index"
 
 describe("military time validator", () => {
   it("should validate a string as a correct military time", () => {
-    const input = "12:12"
+    const input = "12:12 - 12:12"
 
     const result = isValidMilitaryTime(input)
 
@@ -17,29 +17,57 @@ describe("military time validator", () => {
     expect(result).toBeFalsy()
   })
 
-  it("should validate hour in string as a correct", () => {
+  it("should validate hourStart in string as a correct", () => {
     const input = "12:00 - 00:00"
 
     const result = isValidMilitaryTime(input)
 
     expect(result).toBeTruthy()
   })
-  it("should validate hour '24' in string as a correct", () => {
+  it("should validate hourStart '24' in string as a correct", () => {
     const input = "24:00 - 00:00"
 
     const result = isValidMilitaryTime(input)
 
     expect(result).toBeFalsy()
   })
-  it("should validate minutes in string as a correct", () => {
+  it("should validate minuteStart in string as a correct", () => {
     const input = "00:15 - 00:00"
 
     const result = isValidMilitaryTime(input)
 
     expect(result).toBeTruthy()
   })
-  it("should validate minute '60' in string as a correct", () => {
+  it("should validate minuteStart '60' in string as a correct", () => {
     const input = "00:60 - 00:00"
+
+    const result = isValidMilitaryTime(input)
+
+    expect(result).toBeFalsy()
+  })
+  it("should validate hourEnd in string as a correct", () => {
+    const input = "12:00 - 00:00"
+
+    const result = isValidMilitaryTime(input)
+
+    expect(result).toBeTruthy()
+  })
+  it("should validate hourEnd '24' in string as a correct", () => {
+    const input = "24:00 - 24:00"
+
+    const result = isValidMilitaryTime(input)
+
+    expect(result).toBeFalsy()
+  })
+  it("should validate minuteEnd in string as a correct", () => {
+    const input = "00:15 - 00:00"
+
+    const result = isValidMilitaryTime(input)
+
+    expect(result).toBeTruthy()
+  })
+  it("should validate minuteEnd '60' in string as a correct", () => {
+    const input = "00:60 - 00:60"
 
     const result = isValidMilitaryTime(input)
 
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,8 +1,12 @@
 export const isValidMilitaryTime = (timeString: string): boolean => {
   if (timeString.length === 0) return false
-  const hour = parseInt(timeString.substring(0, 2))
-  if (isNaN(hour) || hour < 0 || hour > 23) return false
-  const minute = parseInt(timeString.substring(3, 5))
-  if (isNaN(minute) || minute < 0 || minute > 59) return false
+  const hourStart = parseInt(timeString.substring(0, 2))
+  if (isNaN(hourStart) || hourStart < 0 || hourStart > 23) return false
+  const minuteStart = parseInt(timeString.substring(3, 5))
+  if (isNaN(minuteStart) || minuteStart < 0 || minuteStart > 59) return false
+  const hourEnd = parseInt(timeString.substring(8, 10))
+  if (isNaN(hourEnd) || hourEnd < 0 || hourEnd > 23) return false
+  const minuteEnd = parseInt(timeString.substring(11, 13))
+  if (isNaN(minuteEnd) || minuteEnd < 0 || minuteEnd > 59) return false
   return true
 }
