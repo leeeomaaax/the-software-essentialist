73d5c76032c9a8cf99cb86c52433442b66a73f9b
leomax <leomax89@gmail.com>
2023-04-13 10:42:54 -0300

    NOT operator should invert result

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -7,4 +7,13 @@ describe("boolean calculator", () => {
   it("should evaluate FALSE to false", () => {
     expect(evaluateExpression("FALSE")).toBeFalsy()
   })
+
+  describe("NOT operator should invert result", () => {
+    it("should evaluate NOT TRUE to false", () => {
+      expect(evaluateExpression("NOT TRUE")).toBeFalsy()
+    })
+    it("should evaluate NOT FALSE to true", () => {
+      expect(evaluateExpression("NOT FALSE")).toBeTruthy()
+    })
+  })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,4 +1,7 @@
 export const evaluateExpression = (expression: string): boolean => {
   if (expression === "TRUE") return true
-  return false
+  if (expression === "FALSE") return false
+  if (expression === "NOT TRUE") return false
+  if (expression === "NOT FALSE") return true
+  throw new Error("unhandled expression")
 }
