9dd2e256528c6b8dab6cc9245e4363730aee2834
leomax <leomax89@gmail.com>
2023-04-13 10:59:19 -0300

    AND operator should compose result

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -16,4 +16,30 @@ describe("boolean calculator", () => {
       expect(evaluateExpression("NOT FALSE")).toBeTruthy()
     })
   })
+  describe("AND operator should compose result", () => {
+    it("should evaluate NOT TRUE AND NOT TRUE to false", () => {
+      expect(evaluateExpression("NOT TRUE AND NOT TRUE")).toBeFalsy()
+    })
+    it("should evaluate NOT TRUE AND NOT FALSE to false", () => {
+      expect(evaluateExpression("NOT TRUE AND NOT FALSE")).toBeFalsy()
+    })
+    it("should evaluate NOT FALSE AND NOT FALSE to true", () => {
+      expect(evaluateExpression("NOT FALSE AND NOT FALSE")).toBeTruthy()
+    })
+    it("should evaluate NOT FALSE AND NOT TRUE to false", () => {
+      expect(evaluateExpression("NOT FALSE AND NOT TRUE")).toBeFalsy()
+    })
+    it("should evaluate TRUE AND TRUE to true", () => {
+      expect(evaluateExpression("TRUE AND TRUE")).toBeTruthy()
+    })
+    it("should evaluate TRUE AND FALSE to false", () => {
+      expect(evaluateExpression("TRUE AND FALSE")).toBeFalsy()
+    })
+    it("should evaluate FALSE AND FALSE to false", () => {
+      expect(evaluateExpression("FALSE AND FALSE")).toBeFalsy()
+    })
+    it("should evaluate FALSE AND TRUE to false", () => {
+      expect(evaluateExpression("FALSE AND TRUE")).toBeFalsy()
+    })
+  })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,7 +1,24 @@
+const evaluateExpressionValue = (expressionValue: string): boolean => {
+  if (expressionValue === "TRUE") return true
+  if (expressionValue === "FALSE") return false
+  if (expressionValue === "NOT TRUE") return false
+  if (expressionValue === "NOT FALSE") return true
+
+  throw new Error("unhandled expressionValue")
+}
+
 export const evaluateExpression = (expression: string): boolean => {
   if (expression === "TRUE") return true
   if (expression === "FALSE") return false
   if (expression === "NOT TRUE") return false
   if (expression === "NOT FALSE") return true
-  throw new Error("unhandled expression")
+
+  const expressionValues = expression.split(" AND ")
+
+  const result = expressionValues.reduce(
+    (prevValue, currentValue) =>
+      prevValue && evaluateExpressionValue(currentValue),
+    true
+  )
+  return result
 }
